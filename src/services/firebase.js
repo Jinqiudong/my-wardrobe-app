90713a997c3d4740c9c90f5812eead3f63d8c0a9
jdong338 <jdong338@gatech.edu>
2026-01-04 14:30:08 -0500

    save

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
   authDomain: getEnv('FIREBASE_AUTH_DOMAIN'),
   projectId: getEnv('FIREBASE_PROJECT_ID'),
   storageBucket: getEnv('FIREBASE_STORAGE_BUCKET'),
-  messagingSenderId: getEnv('FIREBASE_MESSAGING_SENDER_ID') || getEnv('FIREBASE_MESSAGE_SENDER_ID'),
+  messagingSenderId: getEnv('FIREBASE_MESSAGING_SENDER_ID'),
   appId: getEnv('FIREBASE_APP_ID')
 };
 
